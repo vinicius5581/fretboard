fc402f01aad2503a87f361d2c82a76c606a7719e
Vinicius Santana <santanaviny@gmail.com>
2020-04-17 01:55:35 -0700

    Updates

---
+++ precache-manifest.1d18056ad532785382d6a90e4df2c491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3248ed6e04cec8c8d3c2a8b97020e934",
+    "url": "/fretboard/index.html"
+  },
+  {
+    "revision": "64ec73b45c6ded35060e",
+    "url": "/fretboard/static/css/main.4774afae.chunk.css"
+  },
+  {
+    "revision": "b93770efcb88e2133e5f",
+    "url": "/fretboard/static/js/2.4bc99db9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fretboard/static/js/2.4bc99db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ec73b45c6ded35060e",
+    "url": "/fretboard/static/js/main.dbb38a5d.chunk.js"
+  },
+  {
+    "revision": "2ed266ae1ad352d8f1bb",
+    "url": "/fretboard/static/js/runtime-main.d67eb460.js"
+  }
+]);
